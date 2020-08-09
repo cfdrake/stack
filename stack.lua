984a077fe1f1958d715e594ce477bcd1ccfe473c
Colin Drake <github@accounts.colindrake.me>
2020-08-09 13:39:30 -0500

    Cleanup code to use params

--- stack.lua
+++ stack.lua
@@ -23,7 +23,6 @@ engine.name = "Stack"
 -- STATE
 -----------------------------
 
-sel = 1
 recording = false
 initital_monitor_level = 0
 
@@ -32,23 +31,8 @@ initital_monitor_level = 0
 -----------------------------
 
 function init()
-  setup_state()
   setup_params()
   setup_pattern()
-end
-
-function setup_state()
-  set_active_filter(1)
-end
-
-function setup_params()
-  initital_monitor_level = params:get('monitor_level')
-  params:set('monitor_level', -math.huge)
-end
-
-function setup_pattern()
-  pattern = pattern_time.new()
-  pattern.process = process_pattern
 end
 
 function cleanup()
@@ -60,8 +44,6 @@ end
 -----------------------------
 
 function set_active_filter(idx)
-  sel = idx
-  
   engine.v1(idx == 1 and 1 or 0)
   engine.v2(idx == 2 and 1 or 0)
   engine.v3(idx == 3 and 1 or 0)
@@ -85,13 +67,28 @@ function filter_hz(idx)
   return math.floor(31.5 * math.pow(2, idx))
 end
 
+function setup_params()
+  initital_monitor_level = params:get('monitor_level')
+  params:set('monitor_level', -math.huge)
+  
+  params:add_number("filter", "Selected Filter", 1, 9, 1)
+  params:set_action("filter", function(idx) set_active_filter(idx) end)
+
+  params:set("filter", 1)  
+  set_active_filter(1)
+end
+
+function setup_pattern()
+  pattern = pattern_time.new()
+  pattern.process = process_pattern
+end
+
 -----------------------------
 -- MIDI/PATTERN HANDLING
 -----------------------------
 
 function process_pattern(ev)
-  sel = ev.idx
-  set_active_filter(sel)
+  params:set("filter", ev.idx)
 end
 
 -----------------------------
@@ -101,8 +98,7 @@ end
 function enc(n, d)
   -- Select filter
   if n == 1 then
-    sel = util.clamp(sel + d, 1, 9)
-    set_active_filter(sel)
+    params:delta("filter", d)
   end
 end
 
@@ -134,18 +130,20 @@ end
 function redraw()
   screen.clear()
   
+  filter = params:get("filter")
+  
   -- Filter index
   screen.move(0, 45)
   screen.font_face(2)
   screen.font_size(40)
   screen.level(15)
-  screen.text(sel)
+  screen.text(filter)
   
   -- Filter hz
   screen.move(20, 45)
   screen.font_size(20)
   screen.level(3)
-  screen.text(filter_hz(sel) .. "hz")
+  screen.text(filter_hz(filter) .. "hz")
   
   -- Recording indicator
   screen.move(20, 25)
