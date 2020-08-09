ce0571c035a7bf9c2c2e7bc31b3028c5de2ad7bb
Colin Drake <github@accounts.colindrake.me>
2020-08-09 13:31:32 -0500

    Clean up code, reduce audio clicks

--- lib/Engine_Stack.sc
+++ lib/Engine_Stack.sc
@@ -7,34 +7,70 @@ Engine_Stack : CroneEngine {
   
   alloc {
     synth = {
-      arg out, sel;
+      arg out, v1 = 0, v2 = 0, v3 = 0, v4 = 0, v5 = 0, v6 = 0, v7 = 1, v8 = 0, v9 = 0;
       
       var inputL = SoundIn.ar(0);
       var inputR = SoundIn.ar(1);
+	
+    	var filtersL = Mix.ar([
+    		BPF.ar(inputL, 63, 0.2, Lag.kr(v1)),
+    		BPF.ar(inputL, 126, 0.2, Lag.kr(v2)),
+    		BPF.ar(inputL, 252, 0.2, Lag.kr(v3)),
+    		BPF.ar(inputL, 504, 0.2, Lag.kr(v4)),
+    		BPF.ar(inputL, 1008, 0.2, Lag.kr(v5)),
+    		BPF.ar(inputL, 2016, 0.2, Lag.kr(v6)),
+    		BPF.ar(inputL, 4032, 0.2, Lag.kr(v7)),
+    		BPF.ar(inputL, 8064, 0.2, Lag.kr(v8)),
+    		BPF.ar(inputL, 16128, 0.2, Lag.kr(v9))]);
+    	
+    	var filtersR = Mix.ar([
+    		BPF.ar(inputR, 63, 0.2, Lag.kr(v1)),
+    		BPF.ar(inputR, 126, 0.2, Lag.kr(v2)),
+    		BPF.ar(inputR, 252, 0.2, Lag.kr(v3)),
+    		BPF.ar(inputR, 504, 0.2, Lag.kr(v4)),
+    		BPF.ar(inputR, 1008, 0.2, Lag.kr(v5)),
+    		BPF.ar(inputR, 2016, 0.2, Lag.kr(v6)),
+    		BPF.ar(inputR, 4032, 0.2, Lag.kr(v7)),
+    		BPF.ar(inputR, 8064, 0.2, Lag.kr(v8)),
+    		BPF.ar(inputR, 16128, 0.2, Lag.kr(v9))]);
       
-      var base = 31.5;
-      var numFilters = 9;
-      
-      var filtersL = Array.fill(numFilters, { | i|
-        var index = 2 ** (i + 1);
-        var freq = base * index;
-        BPF.ar(inputL, freq, 0.2)
-      });
-      
-      var filtersR = Array.fill(numFilters, { | i|
-        var index = 2 ** (i + 1);
-        var freq = base * index;
-        BPF.ar(inputR, freq, 0.2)
-      });
-      
-      var selectedL = LinSelectX.ar(sel, filtersL);
-      var selectedR = LinSelectX.ar(sel, filtersR);
-      
-      Out.ar(out, [selectedL, selectedR]);
+      Out.ar(out, [filtersL, filtersR]);
     }.play(args: [\out, context.out_b], target: context.xg);
   
-    this.addCommand("sel", "i", { arg msg;
-      synth.set(\sel, msg[1]);
+    this.addCommand("v1", "f", { arg msg;
+      synth.set(\v1, msg[1]);
+    });
+    
+    this.addCommand("v2", "f", { arg msg;
+      synth.set(\v2, msg[1]);
+    });
+    
+    this.addCommand("v3", "f", { arg msg;
+      synth.set(\v3, msg[1]);
+    });
+    
+    this.addCommand("v4", "f", { arg msg;
+      synth.set(\v4, msg[1]);
+    });
+    
+    this.addCommand("v5", "f", { arg msg;
+      synth.set(\v5, msg[1]);
+    });
+    
+    this.addCommand("v6", "f", { arg msg;
+      synth.set(\v6, msg[1]);
+    });
+    
+    this.addCommand("v7", "f", { arg msg;
+      synth.set(\v7, msg[1]);
+    });
+    
+    this.addCommand("v8", "f", { arg msg;
+      synth.set(\v8, msg[1]);
+    });
+    
+    this.addCommand("v9", "f", { arg msg;
+      synth.set(\v9, msg[1]);
     });
   }
   
--- stack.lua
+++ stack.lua
@@ -8,10 +8,14 @@
 -- Press KEY2 then KEY3 to stop pattern (records empty pattern)
 
 -----------------------------
--- INCLUDES, ETC.
+-- INCLUDES
 -----------------------------
 
 local pattern_time = require "pattern_time"
+
+-----------------------------
+-- ENGINE
+-----------------------------
 
 engine.name = "Stack"
 
@@ -19,7 +23,8 @@ engine.name = "Stack"
 -- STATE
 -----------------------------
 
-sel = 0
+sel = 1
+recording = false
 initital_monitor_level = 0
 
 -----------------------------
@@ -27,11 +32,21 @@ initital_monitor_level = 0
 -----------------------------
 
 function init()
-  sel = 0
-  
+  setup_state()
+  setup_params()
+  setup_pattern()
+end
+
+function setup_state()
+  set_active_filter(1)
+end
+
+function setup_params()
   initital_monitor_level = params:get('monitor_level')
   params:set('monitor_level', -math.huge)
-  
+end
+
+function setup_pattern()
   pattern = pattern_time.new()
   pattern.process = process_pattern
 end
@@ -41,13 +56,42 @@ function cleanup()
 end
 
 -----------------------------
+-- HELPERS
+-----------------------------
+
+function set_active_filter(idx)
+  sel = idx
+  
+  engine.v1(idx == 1 and 1 or 0)
+  engine.v2(idx == 2 and 1 or 0)
+  engine.v3(idx == 3 and 1 or 0)
+  engine.v4(idx == 4 and 1 or 0)
+  engine.v5(idx == 5 and 1 or 0)
+  engine.v6(idx == 6 and 1 or 0)
+  engine.v7(idx == 7 and 1 or 0)
+  engine.v8(idx == 8 and 1 or 0)
+  engine.v9(idx == 9 and 1 or 0)
+  
+  if recording then
+    ev = {}
+    ev.idx = idx
+    pattern:watch(ev)
+  end
+  
+  redraw()
+end
+
+function filter_hz(idx)
+  return math.floor(31.5 * math.pow(2, idx))
+end
+
+-----------------------------
 -- MIDI/PATTERN HANDLING
 -----------------------------
 
 function process_pattern(ev)
-  sel = ev.sel
-  engine.sel(sel)
-  redraw()
+  sel = ev.idx
+  set_active_filter(sel)
 end
 
 -----------------------------
@@ -55,21 +99,16 @@ end
 -----------------------------
 
 function enc(n, d)
+  -- Select filter
   if n == 1 then
-    sel = util.clamp(sel + d, 0, 8)
-    
-    ev = {}
-    ev.sel = sel
-        
-    pattern:watch(ev)
+    sel = util.clamp(sel + d, 1, 9)
+    set_active_filter(sel)
   end
-  
-  engine.sel(sel)
-  redraw()
 end
 
 function key(n, z)
   if n == 2 then
+    -- Start recording
     if recording then
       return
     end
@@ -79,6 +118,7 @@ function key(n, z)
     pattern:rec_start()
     recording = true
   elseif n == 3 then
+    -- Start playback
     pattern:rec_stop()
     pattern:start()
     recording = false
@@ -93,18 +133,25 @@ end
 
 function redraw()
   screen.clear()
+  
+  -- Filter index
   screen.move(0, 45)
   screen.font_face(2)
   screen.font_size(40)
   screen.level(15)
-  screen.text(sel + 1)
+  screen.text(sel)
+  
+  -- Filter hz
   screen.move(20, 45)
   screen.font_size(20)
   screen.level(3)
-  screen.text(math.floor(31.5 * math.pow(2, sel + 1)) .. "hz")
+  screen.text(filter_hz(sel) .. "hz")
+  
+  -- Recording indicator
   screen.move(20, 25)
   screen.font_size(10)
   screen.level(3)
   screen.text(recording and "recording" or "")
+  
   screen.update()
 end
